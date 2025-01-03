5bc6b60b7390d1e77f032dd2a3b306f32f2d7620
Haritha-msk <mskharitha98@gmail.com>
2025-01-03 22:33:20 +0530

    Update streamlit_app.py
    Fetched the data from the api with the chosen name

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,14 @@ if name_on_order:
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
-#st.dataframe(data= my_dataframe, use_container_width= True)
+# st.dataframe(data= my_dataframe, use_container_width= True)
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -31,6 +36,8 @@ if ingredients_list:
     ingredients_string = ''
     for x in ingredients_list:
         ingredients_string += x+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == x, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', x,' is ', search_on, '.')
         st.subheader(x+' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+x)
         sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width= True)
